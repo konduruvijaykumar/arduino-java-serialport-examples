03fc18cc41dce9838032df3fb84e7583e35c2266
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-06-11 11:19:09 +0530

    Vijay - 2017/06/10 - adding arduino java serialport examples

---
+++ DHT22/JavaSerialPortReadingArduino/src/org/pjay/serialportreading/arduino/SerialPortReadingApp.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.serialportreading.arduino;
+
+/**
+ * @author Vijay
+ * http://playground.arduino.cc/Interfacing/Java
+ * https://www.youtube.com/watch?v=43Vdpz1YmdU
+ * http://rxtx.qbang.org/wiki/index.php/Main_Page
+ * http://fizzed.com/oss/rxtx-for-java
+ * http://stackoverflow.com/questions/9044758/gnu-io-portinuseexception-unknown-application
+ *
+ */
+public class SerialPortReadingApp {
+
+}
